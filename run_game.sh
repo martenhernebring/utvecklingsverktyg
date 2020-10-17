64639e46abf278c08102ef1361e3119f09f2423f
Marten Hernebring <marten.hernebring@gmail.com>
2020-10-17 20:11:28 +0200

    Created bash-file run_game.sh and wrote folder name

---
+++ run_game.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Uppgift 4 - Skapa ett skript run_game.sh
+#1. Skriva ut ert namn
+echo Marten_Hernebring_labb
+#2. Skapa en katalog med ert namn och suffixet _labb i mitt fall blir katalogen Nahid_Vafaie_labb använd _ (underscore) i stället för mellanslag!
+#3. Kopiera alla java-filer till katalogen
+#4. Byt katalog i skriptet till katalogen ovan (den katalog som beskrevs i texten ovanför)
+#5. Skriv ut aktuell katalog med pwd för att jag ska se att ni lyckats skapa och byta katalog
+#6. skriv ut att ni ska kompilera- kompilera
+#7. Skriv ut att ni kör programmet (“Running game…”)- kör programmet
+#8. Skriv ut att programmet är färdigt (“Done”)
+#9. Ta bort alla class-filer
+#10. Lista katalogen så att jag kan se att class-filerna är borta
